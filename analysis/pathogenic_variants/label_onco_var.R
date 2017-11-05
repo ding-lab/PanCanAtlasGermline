688f46c7396d77526db924dde65e715a22674b72
Kuan Huang <huangkuanlin@gmail.com>
2017-11-04 21:13:56 -0500

    scripts to annotate/filter/plot pathogenic cancer variants

---
+++ analysis/pathogenic_variants/label_onco_var.R
@@ -0,0 +1,201 @@
+##### label_onco_var.R #####
+# Kuan-lin Huang @ WashU 2017 Oct
+# find non-cancer pathogenic variant in the Pan8000 cohort
+
+bdir = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/analysis/pathogenic_variants"
+setwd(bdir)
+source("../global_aes_out.R")
+
+
+fn = "/Users/khuang/Box Sync/PhD/germline/PanCanAtlasGermline/analysis/process_files/germline/local/ChargedSample/charged.PCA.r1.TCGAbarcode.merge.exon.ALL.vcf.samples.expanded.AFcorrected.lowAF.sele.tsv"
+variants = read.table(sep="\t",header=T,file=fn, stringsAsFactors=FALSE, quote = "",fill=TRUE)
+
+gene_fn = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/TCGA_data/reference_files/20160713_Rahman_KJ_KH_152_gene_table_list.txt"
+predisposition_genes = as.vector(t(read.table(sep="\t",header=F,file=gene_fn, stringsAsFactors=FALSE, quote = "")))
+
+samples_fn = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/TCGA_data/sampleQC/pca_table.20171019.wclin.tsv"
+samples = read.table(sep="\t",header=T,file=samples_fn, stringsAsFactors=FALSE, quote = "")
+# samples_brief = samples[,1:5]
+# colnames(samples_brief)[2] = "Sample"
+
+bam_location_fn = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/TCGA_data/sampleQC/pca_table_with_tumorBams.20171019.patched_28oct.tsv"
+bam_location = read.table(sep="\t",header=T,file=bam_location_fn, stringsAsFactors=FALSE, quote = "", fill=T)
+bam_location_s = bam_location[bam_location$sbarcode %in% samples$bcr_patient_barcode,]
+bam_location_s_brief = bam_location_s[,c(1:5,8:10)]
+colnames(bam_location_s_brief)[4] = "Sample"
+colnames(bam_location_s_brief)[1:2] = c("bcr_patient_barcode","cancer")
+
+# subset to PCA germline project sample
+#variants$bcr_patient_barcode = substring(variants$Sample,1,12)
+cat("Original count of variants: ",nrow(variants),"\n")
+variants_pca = merge(variants,bam_location_s_brief,by="Sample",all=F)
+cat("PCA sample sets of 10,467, # of variants left: ",nrow(variants_pca),"\n")
+
+##### Add readcounts and readcount filter #####
+# snp_rc_fn = "/Users/khuang/Box Sync/PhD/germline/PanCanAtlasGermline/analysis/process_files/germline/local/ChargedSample/single_sub.rc_anno.tsv"
+# snp_rc = read.table(sep="\t", header=T, file=snp_rc_fn, stringsAsFactors=FALSE, quote = "",fill=T)
+# del_rc_fn = "/Users/khuang/Box Sync/PhD/germline/PanCanAtlasGermline/analysis/process_files/germline/local/ChargedSample/del.rc_anno.tsv"
+# del_rc = read.table(sep="\t", header=T, file=del_rc_fn , stringsAsFactors=FALSE, quote = "",fill=T)
+# all_rc = rbind(snp_rc,del_rc)
+all_rc_fn = "/Users/khuang/Box Sync/PhD/germline/PanCanAtlasGermline/analysis/process_files/germline/local/ChargedSample/all.rc_anno"
+all_rc = read.table(sep="\t", header=T, file=all_rc_fn , stringsAsFactors=FALSE, quote = "",fill=T)
+all_rc[all_rc==-1]=NA # swap out -1 for NA to avoid confusion
+all_rc$altBase = gsub("\\-","del",all_rc$altBase)
+all_rc$altBase = gsub("\\+","ins",all_rc$altBase)# excel friendly format
+variants_pca = merge(variants_pca,all_rc,all.x = T, by=c("Sample","HGVSg"))
+variants_pca$normalVAF = variants_pca$normalAltCnt/variants_pca$normalDepth
+variants_pca$tumorVAF = variants_pca$tumorAltCnt/variants_pca$tumorDepth
+
+p = ggplot(data=variants_pca)
+p = p + geom_point(aes(x=normalVAF,y=tumorVAF), alpha = 0.02,stroke=0)
+p = p + theme_bw()
+p = p + theme(axis.text.x = element_text(colour="black", size=8,angle=90,vjust=0.5))#,
+p
+fn = "out/normal_tumor_VAF_prefilter.pdf"
+ggsave(file=fn, h=5,w=5,useDingbats=FALSE)
+
+p = ggplot(data=variants_pca)
+p = p + geom_point(aes(x=normalAltCnt,y=tumorAltCnt), alpha = 0.02,stroke=0)
+p = p + theme_bw() + xlim(0,50) + ylim(0,50)
+p = p + theme(axis.text.x = element_text(colour="black", size=8,angle=90,vjust=0.5))#,
+p
+fn = "out/normal_tumor_altcnt_prefilter.pdf"
+ggsave(file=fn, h=5,w=5,useDingbats=FALSE)
+
+variants_pca = variants_pca[is.na(variants_pca$normalAltCnt) | (variants_pca$normalAltCnt > 4),]
+cat("Applying normal Alt readcount >= 5, # of variants left: ",nrow(variants_pca),"\n")
+variants_pca = variants_pca[is.na(variants_pca$tumorAltCnt) | (variants_pca$tumorAltCnt > 4),]
+cat("Applying tumor Alt readcount >= 5, # of variants left: ",nrow(variants_pca),"\n")
+
+variants_pca = variants_pca[is.na(variants_pca$normalVAF) | (variants_pca$normalVAF >= 0.2),]
+cat("Applying normal Alt readcount >= 0.2, # of variants left: ",nrow(variants_pca),"\n")
+variants_pca = variants_pca[is.na(variants_pca$tumorVAF) | (variants_pca$tumorVAF >= 0.2),]
+cat("Applying tumor Alt readcount >= 0.2, # of variants left: ",nrow(variants_pca),"\n")
+
+p = ggplot(data=variants_pca)
+p = p + geom_point(aes(x=normalAltCnt,y=tumorAltCnt), alpha = 0.02,stroke=0)
+p = p + theme_bw() + xlim(0,50) + ylim(0,50)
+p = p + theme(axis.text.x = element_text(colour="black", size=8,angle=90,vjust=0.5))#,
+p
+fn = "out/normal_tumor_altcnt_postfilter.pdf"
+ggsave(file=fn, h=5,w=5,useDingbats=FALSE)
+
+p = ggplot(data=variants_pca)
+p = p + geom_point(aes(x=normalVAF,y=tumorVAF), alpha = 0.02,stroke=0)
+p = p + theme_bw()
+p = p + theme(axis.text.x = element_text(colour="black", size=8,angle=90,vjust=0.5))#,
+p
+fn = "out/normal_tumor_VAF_postfilter.pdf"
+ggsave(file=fn, h=5,w=5,useDingbats=FALSE)
+
+##### classify whether a variant is cancer-relevant #####
+cancer_terms = c("tumor","cancer","oma","neoplasia")
+
+variants_pca$predisposition_gene = F
+variants_pca$predisposition_gene[variants_pca$HUGO_Symbol %in% predisposition_genes] = TRUE
+variants_pca$cancer_term_trait = FALSE
+for (term in cancer_terms){
+  variants_pca$cancer_term_trait[grep(term,tolower(variants_pca$ClinVar_Traits))] = TRUE
+}
+table(variants_pca$predisposition_gene,variants_pca$cancer_term_trait)
+variants_pca$cancer_related = F
+variants_pca$cancer_related[variants_pca$predisposition_gene | variants_pca$cancer_term_trait] = T
+
+# variant frequency annotation
+var_freq = data.frame(table(variants_pca$HGVSg))
+colnames(var_freq) = c("HGVSg","Cohort_AC")
+variants_pca_frq = merge(variants_pca,var_freq,by="HGVSg")
+variants_pca_frq$Cohort_AF = variants_pca_frq$Cohort_AC/nrow(samples)
+# dim(variants_pca_frq[variants_pca_frq$Cohort_AF > 0.01,])
+
+tn = "charged.PCA.r1.TCGAbarcode.merge.exon.ALL.vcf.samples.cleaned.expanded.AFcorrected.lowAF.sele.labeled.tsv"
+write.table(variants_pca_frq, quote=F, sep="\t", file = tn, row.names = F)
+
+# table(variants_pca_frq$Cohort_AF < 0.0005,variants_pca_frq$cancer_related) 
+# sum(variants_pca_frq$Cohort_AF < 0.0005)
+# table(variants_pca_frq$Cohort_AF < 0.01,variants_pca_frq$cancer_related)
+
+# all truncations and missense with less cohort frequency
+# table(variants_pca_frq$Consequence)
+# table(variants_pca_frq$Variant_Classification)
+
+# ##### check against previous variants #####
+# pan8000_fn = "/Users/khuang/Box\ Sync/PhD/germline/pan8000_germline_clinical/functional_clinical_integration/tables/2016-07-18/2016-07-18_KH_pan8000_MAF0.05_germline_class_func_clin_somatic_PLP_only.txt"
+# pan8000 = read.table(sep="\t",header=T,file=pan8000_fn, stringsAsFactors=FALSE, quote = "")
+# pan8000$pan8000_sample_var = paste(pan8000$sample,pan8000$Start)  
+# 
+# pca_var = paste(variants_pca_frq$bcr_patient_barcode,variants_pca_frq$Start)
+# length(pan8000$pan8000_sample_var)
+# length(pan8000$pca_var)
+# sum(pan8000$pan8000_sample_var %in% pca_var)
+# sum(pca_var %in% pan8000$pan8000_sample_var)
+# not_found = pan8000[!(pan8000$pan8000_sample_var %in% pca_var),]
+# not_found = not_found[order(not_found$start),]
+# not_found_trun =not_found[not_found$Variant_Classification=="frameshift_variant",]
+
+##### rare frequency filter #####
+variants_pca_frq_rare_1000G = variants_pca_frq[is.na(variants_pca_frq$GMAF) | variants_pca_frq$GMAF < 0.0005,]
+variants_pca_frq_rare_1000G_ExAC = variants_pca_frq_rare_1000G[is.na(variants_pca_frq_rare_1000G$ExAC_adj_AF) | variants_pca_frq_rare_1000G$ExAC_adj_AF < 0.0005,]
+# have to manually check these variants
+unique(variants_pca_frq_rare_1000G_ExAC$HGVSg[variants_pca_frq_rare_1000G_ExAC$Cohort_AF > 0.001 & variants_pca_frq_rare_1000G_ExAC$Variant_Classification != "missense_variant" & variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF==0])
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual = NA
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="11:g.22646872_22646873delAG" ] = 	0.00008343
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="12:g.121432117_121432118insC"] = 0.001854
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="13:g.32914438_32914438delT"] =	0.0002651
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="15:g.91303325"] = 0.4311
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="16:g.2096132_2096133insG" ] =		0.0001745
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="16:g.89805421_89805427insAACAG"] =	0.002389
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="17:g.41209079_41209080insG" ] =		0.0001565
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="17:g.63532584_63532585insC" ] =	0.0005561
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="2:g.48030639_48030642insCC"] =		0.000008310
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="2:g.48033791"] =		0.001293
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="6:g.32007887G>T"] =		0.01042
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="12:g.12044534"] =	1 # low coverage
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="18:g.42456664_42456665insTCTC"] =	1 # low coverage
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="2:g.48030639_48030642insCC"] =	0.001753
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="2:g.241808314C>G"] =	0.1545
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="2:g.190670539_190670543insAAA"] =	0.2434
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="3:g.142274739_142274742insTT"] =	0.003416
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="9:g.36882049_36882050insG"] =	0.0001290
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="9:g.37020768_37020769insC" ] =		0.0001402
+variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual[variants_pca_frq_rare_1000G_ExAC$HGVSg=="9:g.98209616_98209617insG" ] =		0.0001554
+
+variants_pca_frq_rare_1000G_ExAC_pass = variants_pca_frq_rare_1000G_ExAC[is.na(variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual) | variants_pca_frq_rare_1000G_ExAC$ExAC_adj_AF_Manual < 0.0005,]
+## don't do cohort freq for now
+# variants_pca_frq_rare_1000G_ExAC_pass_001 = variants_pca_frq_rare_1000G_ExAC_pass[variants_pca_frq_rare_1000G_ExAC_pass$Cohort_AF< 0.01,]
+# tail(table(variants_pca_frq_rare_1000G_ExAC_pass_001$HGVSg)[order(table(variants_pca_frq_rare_1000G_ExAC_pass_001$HGVSg))])[2:5]
+
+variants_pca_frq_rare_1000G_ExAC_pass_cancer = variants_pca_frq_rare_1000G_ExAC_pass[variants_pca_frq_rare_1000G_ExAC_pass$cancer_related,]
+
+# p = ggplot(data=variants_pca_frq_rare_1000G_ExAC_pass_cancer)
+# p = p + geom_histogram(aes(x=Cohort_AF, fill=Variant_Classification),binwidth = 0.0005)
+# #p = p + geom_text(aes(x=Cancer,y=Count, label=ifelse(Technology=="WXS",Count,NA)),color="black", vjust = -0.5, size=2)
+# p = p + theme_bw()# + xlim(0,0.1) #+ ylim(0,0.1)
+# p = p + theme(axis.text.x = element_text(colour="black", size=8,angle=90,vjust=0.5))#,
+# p
+# fn = "out/variants_pca_frq_rare_1000G_ExAC_pass_cancer_cohortAF.pdf"
+# ggsave(file=fn, h=5,w=6,useDingbats=FALSE)
+
+
+##### for manual review ##### 
+
+## previous manual review results ##
+prev_truncation_f = "/Users/khuang/Box\ Sync/PhD/germline/pan8000_germline_clinical/variant_files/201509_pan8000_0.05percentMAF/truncation/pan8000.truncation.var.1percentMAF.remove-low-coverage_0.05percentMAF_624_genes_rc.txt"
+prev_pass_truncation_f = "/Users/khuang/Box\ Sync/PhD/germline/pan8000_germline_clinical/variant_files/201509_pan8000_0.05percentMAF/truncation/pan8000.truncation.var.1percentMAF.remove-low-coverage_0.05percentMAF_624_genes_rc_oldManualRev_manualRev_cleaned_germline_noNonstop.txt"
+prev_truncation = read.table(sep="\t",header=F,file=prev_truncation_f, stringsAsFactors=FALSE, quote = "",fill=TRUE)
+prev_pass_truncation = read.table(sep="\t",header=T,file=prev_pass_truncation_f, stringsAsFactors=FALSE, quote = "",fill=TRUE)
+all = paste(prev_truncation$V22,prev_truncation$V2,sep=":")
+pass = paste(prev_pass_truncation$sample,prev_pass_truncation$start,sep=":")
+fail = all[!(all %in% pass)]
+
+variants_pca_frq_rare_1000G_ExAC_pass_cancer$previous_manual_review = NA
+variants_pca_frq_rare_1000G_ExAC_pass_cancer$previous_manual_review[paste(variants_pca_frq_rare_1000G_ExAC_pass_cancer$bcr_patient_barcode,variants_pca_frq_rare_1000G_ExAC_pass_cancer$Start,sep=":") %in% pass] = "Pass"
+variants_pca_frq_rare_1000G_ExAC_pass_cancer$previous_manual_review[paste(variants_pca_frq_rare_1000G_ExAC_pass_cancer$bcr_patient_barcode,variants_pca_frq_rare_1000G_ExAC_pass_cancer$Start,sep=":") %in% fail] = "Fail"
+
+
+tn = "charged.PCA.r1.TCGAbarcode.merge.exon.ALL.vcf.samples.cleaned.expanded.AFcorrected.lowAF.sele.labeled.rare.cancer.tsv"
+write.table(variants_pca_frq_rare_1000G_ExAC_pass_cancer, quote=F, sep="\t", file = tn, row.names = F)
+
+# table(variants_pca_frq_rare_1000G_ExAC_pass_cancer$Variant_Classification)
+# length(unique(variants_pca_frq_rare_1000G_ExAC_pass_cancer$bcr_patient_barcode))
+# length(unique(variants_pca_frq_rare_1000G_ExAC_pass_cancer[variants_pca_frq_rare_1000G_ExAC_pass_cancer$Cohort_AF < 0.0005,]$bcr_patient_barcode))
---
+++ analysis/pathogenic_variants/plot_path_var_dist.R
@@ -0,0 +1,154 @@
+##### plot_path_var_dist.R #####
+# Kuan-lin Huang @ WashU 2017 Oct
+# plot the distribution of pathogenic variants
+
+### dependencies ###
+bdir = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/analysis/pathogenic_variants"
+setwd(bdir)
+source("../dependency_files.R")
+source("../global_aes_out.R")
+
+## functions 
+plot_barplot = function(matrix, x_string, fill_string=NULL, fileName="data.pdf"){
+  
+  fn = paste("out", fileName,sep ="/")
+  
+  if (is.null(fill_string)){
+    p = ggplot(matrix,aes_string(x = x_string))
+  } else{
+    p = ggplot(matrix,aes_string(x = x_string, fill = fill_string))
+  }
+  
+  p = p + geom_bar() + theme_bw() + theme_nogrid()
+  p = p + labs(x = x_string, y="counts")
+  p = p + theme(axis.title = element_text(size=12), axis.text.x = element_text(colour="black", size=10, angle = 90, vjust=0.5), axis.text.y = element_text(colour="black", size=12))#element_text(colour="black", size=14))
+  p
+  ggsave(file=fn, useDingbats=FALSE)
+}
+
+plot_heatmap = function(data){
+  getPalette = colorRampPalette(c("#FFFFFF","#fed976","#e31a1c"))
+  p = ggplot(data=data)
+  p = p + facet_grid(.~classification,drop=T,scales = "free", space = "free")
+  p = p + geom_tile(aes(x=cancer, y=gene, fill=count), linetype="blank") + scale_fill_gradientn(name= "Count", colours=getPalette(100), na.value=NA, limit=c(0,NA))
+  p = p + geom_text(aes(x=cancer, y=gene, label = count, stringsAsFactors=FALSE), color="black", size=3)
+  p = p  + theme_bw() + theme_nogrid() +
+    theme(axis.title = element_text(size=16), axis.text.x = element_text(colour="black", size=12, angle=90, vjust = 0.5), axis.text.y = element_text(colour="black", size=14),axis.ticks = element_blank())#element_text(colour="black", size=14))
+  return(p)
+}
+
+# CharGer classification file
+var_f = "charged.PCA.r1.TCGAbarcode.merge.exon.ALL.vcf.samples.cleaned.expanded.AFcorrected.lowAF.sele.labeled.rare.cancer.tsv"
+
+# other files
+cancer_f = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/analysis/sample_listing/out/cancer_count_wethni_waao.txt" 
+geneList_f = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/TCGA_data/reference_files/20160713_Rahman_KJ_KH_152_gene_table_list.txt"
+
+##### READ inputs #####
+variants = read.table(sep="\t",header=T,file=var_f, stringsAsFactors=FALSE, quote = "",fill=TRUE)
+
+cancer_clin = read.table(header=T,sep="\t",file = cancer_f)
+cancer_count = cancer_clin[,c("Cancer", "Sample.size")]
+colnames(cancer_count)[2] = "sample_size"
+
+geneList_t = read.table(header=F, file = geneList_f)
+geneList = as.vector(t(geneList_t))
+
+##### preliminary plotting #####
+
+plot_barplot(variants,x_string = "cancer", fill_string = "CharGer_Classification", 
+             fileName="PCA_PathVar_Classification_by_cancer_bar.pdf")
+
+
+# Frequency plotting #
+# get non-redundant sample entry first
+variants_uni = variants[!duplicated(variants$Sample),]
+
+# plot the bar plot of the counts
+all_count = as.data.frame(table(variants_uni$cancer,variants_uni$CharGer_Classification))
+colnames(all_count) = c("Cancer", "CharGer_Classification", "count")
+all_count_m = merge(cancer_count, all_count, by="Cancer")
+all_count_m$freq = all_count_m$count/all_count_m$sample_size
+all_count_m$CharGer_Classification = factor(all_count_m$CharGer_Classification, levels = c("Pathogenic","Likely Pathogenic"))
+all_count_m = all_count_m[order(all_count_m$CharGer_Classification),]
+
+all_count_m_sum = dcast(all_count_m, Cancer ~ CharGer_Classification, value.var="freq")
+cancer_order = all_count_m_sum$Cancer[order(all_count_m_sum$Pathogenic, decreasing=TRUE)]
+
+all_count_m$Cancer = factor(all_count_m$Cancer, levels=cancer_order)
+all_count_m= all_count_m[all_count_m$freq !=0,]
+all_count_m$CharGer_Classification = factor(all_count_m$CharGer_Classification, levels=c("Likely Pathogenic","Pathogenic"))
+
+p = ggplot(all_count_m,aes(x = Cancer, y = freq*100, fill = CharGer_Classification))
+p = p + geom_bar(stat = "identity") + theme_bw() + theme_nogrid() 
+p = p + labs(x = "Cancer Type", y="Percentage Carriers (%)")
+p = p + theme(axis.title = element_text(size=12), axis.text.x = element_text(colour="black", size=10, angle = 90, vjust=0.5), axis.text.y = element_text(colour="black", size=12))#element_text(colour="black", size=14))
+p = p + theme(legend.position = "top")
+p
+fn = 'out/PCA_carrierFreq_by_cancer_bar.pdf'
+ggsave(file=fn, useDingbats=FALSE)
+
+##### compare counts to Lu et al. #####
+# lu et al. discovery strategy
+lu_f = "/Users/khuang/Box\ Sync/PhD/germline/pan4000/ncomms10086-s3_lu_et_al_838var.txt"
+lu = read.table(header=T,file=lu_f, sep="\t")
+
+lu_34_burden_gene_f = "/Users/khuang/Box\ Sync/PhD/germline/pan4000/burden_test/34_FDR0.15genes_oncoRemoved.list"
+lu_34_burden_gene = as.vector(t(read.table(header=F,lu_34_burden_gene_f)))
+
+lu_sample_f = "/Users/khuang/Box\ Sync/PhD/germline/pan4000/mis/figure5c_1.data.tsv"
+lu_sample = as.vector(read.table(header=T,lu_sample_f)[,1])
+
+# variants_p_34 = variants_p[(variants_p$sample %in% lu_sample) & (variants_p$gene_name %in% lu_34_burden_gene) & (variants_p$binary_type=="Truncation"),]
+variants_in_lu = variants[(variants$bcr_patient_barcode %in% lu_sample) & (variants$Start %in% lu$start),]
+cat("Total numbers of pathogenic variants:", nrow(variants),"\n")
+cat("Total numbers of pathogenic variants reported in Lu et al:", nrow(variants_in_lu),"\n")
+
+lu_count = as.data.frame(table(variants_in_lu$cancer))
+colnames(lu_count) = c("cancer","previously_reported")
+huang_count = as.data.frame(table(variants$cancer))
+colnames(huang_count) = c("cancer","total")
+both_counts = merge(lu_count,huang_count,by="cancer",all=T)
+both_counts$previously_reported[is.na(both_counts$previously_reported)]=0
+both_counts$novel = both_counts$total - both_counts$previously_reported
+both_counts_m = melt(both_counts,id.vars =c("cancer","total"))
+both_counts_m$cancer = factor(both_counts_m$cancer, levels=cancer_order)
+both_counts_m$variable = factor(both_counts_m$variable, levels=c("novel","previously_reported"))
+
+p = ggplot(both_counts_m,aes(x = cancer, y = value, fill = variable))
+p = p + geom_bar(stat = "identity") + theme_bw() + theme_nogrid() 
+p = p + labs(x = "Cancer Type", y="Pathogenic Variant Count")
+p = p + theme(axis.title = element_text(size=12), axis.text.x = element_text(colour="black", size=10, angle = 90, vjust=0.5), axis.text.y = element_text(colour="black", size=12))#element_text(colour="black", size=14))
+p = p + theme(legend.position = "top")
+p
+fn = 'out/previous_vs_novel_var_count.pdf'
+ggsave(file=fn, height = 5, width = 5, useDingbats=FALSE)
+
+##### heatmap: count of pathogenic variants by gene/cancer #####
+combined_sum_f_added = data.frame(table(variants$HUGO_Symbol, variants$cancer))
+colnames(combined_sum_f_added) = c("Gene","Cancer","Count")
+combined_sum_f_added = combined_sum_f_added[combined_sum_f_added$Count != 0,]
+combined_sum_f_added$Gene_Classification = NA
+combined_sum_f_added$Gene_Classification[combined_sum_f_added$Gene %in% all_oncogenes] = "Oncogene"
+combined_sum_f_added$Gene_Classification[combined_sum_f_added$Gene %in% all_TSGs] = "TSG"
+
+top = table(variants$HUGO_Symbol)[order(table(variants$HUGO_Symbol),decreasing = T)]
+top5 = top[top>4]
+top_oncogenes = names(top[top>7])[names(top[top>7]) %in% all_oncogenes]
+top_TSGs = names(top[top>19])[names(top[top>19]) %in% all_TSGs]
+write(c(top_oncogenes,top_TSGs), "/Users/khuang/Box Sync/PhD/germline/PanCanAtlasGermline/TCGA_data/reference_files/PCA_feature_gene_list.txt", sep="\n")
+
+combined_sum_f_added_g = combined_sum_f_added[combined_sum_f_added$Gene %in% c(top_oncogenes,top_TSGs),]
+combined_sum_f_added_g$Cancer = factor(combined_sum_f_added_g$Cancer, levels=cancer_order) # site from plot_classification_summary.R
+
+getPalette = colorRampPalette(c("#FFFFFF","#fed976","#e31a1c"))
+
+p = ggplot(data=combined_sum_f_added_g)
+p = p + facet_grid(Gene_Classification~.,drop=T,scales = "free_y", space = "free_y")
+p = p + geom_tile(aes(x=Cancer, y=Gene, fill=Count), linetype="blank") + scale_fill_gradientn(name= "Count", colours=getPalette(100), na.value=NA, limit=c(0,NA))
+p = p + geom_text(aes(x=Cancer, y=Gene, label = Count), color="black", size=3)
+p = p  + theme_bw() + theme_nogrid() +
+  theme(axis.title = element_text(size=16), axis.text.x = element_text(colour="black", size=12, angle=90, vjust = 0.5), axis.text.y = element_text(colour="black", size=12),axis.ticks = element_blank())#element_text(colour="black", size=14))
+p
+fn = 'out/PathVar_counts_ggroup_heatmap.pdf'
+ggsave(file=fn, height=7, width=7, useDingbats=FALSE)
