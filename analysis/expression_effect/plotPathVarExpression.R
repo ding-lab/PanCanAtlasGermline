b543445f1b3ae151e0e18ed7dc974e93ccd0cdf5
Kuan Huang <huangkuanlin@gmail.com>
2017-11-15 01:04:14 -0600

    updated scripts to do germline-expression association stats/plot

--- analysis/expression_effect/plotPathVarExpression.R
+++ analysis/expression_effect/plotPathVarExpression.R
@@ -9,7 +9,7 @@ source("../dependency_files.R")
 
 p = ggplot(pathVarOT,aes(x=0,y=expressionQuantile, fill=binary_type))
 p = p + facet_grid(.~Gene_Classification, scale = "free", space = "free", drop=T)
-p = p + geom_dotplot(dotsize=0.4,binwidth=.02, binaxis= "y")#,stackdir ="centerwhole")
+p = p + geom_dotplot(dotsize=0.4,binwidth=.015, binaxis= "y")#,stackdir ="centerwhole")
 p = p + theme_bw() 
 p = p + guides(fill=FALSE) + ylab("Expression Quantile")
 p = p + scale_y_continuous(breaks = seq(0,1, by= 0.25))
@@ -20,7 +20,20 @@ p = p + theme(axis.title = element_text(size=16),
               legend.position = "bottom")#element_text(colour="black", size=14))
 p
 fn = "out/pathVarExpression.pdf"
-ggsave(file=fn, w=6, h=6,useDingbats=FALSE)
+ggsave(file=fn, w=7, h=6,useDingbats=FALSE)
+
+pathVarFGene =pathVarFGene[!is.na(pathVarFGene$binary_type),]
+p = ggplot(pathVarFGene,aes(x=HUGO_Symbol,y=expressionQuantile, fill=binary_type))
+p = p + facet_grid(.~Gene_Classification, scale = "free", space = "free", drop=T)
+p = p + geom_dotplot(dotsize=1.2,binwidth=.01, binaxis= "y",colour=NA,stackdir ="centerwhole")
+p = p + theme_bw() 
+p = p + ylab("mRNA Expression Quantile") + xlab("Gene with Germline Variant")
+p = p + scale_y_continuous(breaks = seq(0,1, by= 0.25))
+#p = p + getVarColorScale()
+p = p + theme(axis.title = element_text(size=12), axis.text.x = element_text(colour="black", size=10, angle = 90, vjust=0.5), axis.text.y = element_text(colour="black", size=12))
+p
+fn = "out/pathVarExpression_byGene.pdf"
+ggsave(file=fn, w=9, h=6,useDingbats=FALSE)
 
 ### Expression vs. CharGer ###
 # first make a dataframe with frequencies
@@ -44,7 +57,7 @@ truncations$bp_to_end = truncations$transcript.length - cDNA_location
 truncations$bp_boolean = TRUE
 truncations[!is.na(truncations$bp_to_end) & truncations$bp_to_end <= 50, ]$bp_boolean = FALSE
 
-p = ggplot(truncations,aes(x=bp_to_end, y =expressionQuantile, color=CharGer_Classification))#, color=Variant_Classification))
+p = ggplot(truncations,aes(x=bp_to_end, y =expressionQuantile, color=Overall_Classification))#, color=Variant_Classification))
 p = p + facet_grid(Variant_Classification~.,drop=T)
 p = p + geom_point(alpha=0.2) 
 p = p + theme_bw() + theme_nogrid() #+ guides(color=FALSE)
@@ -55,9 +68,9 @@ p
 fn = "out/pathVar_trunc_exp_vs_transcript_bp.pdf"
 ggsave(file=fn, useDingbats=FALSE)
 
-truncations=truncations[!is.na(truncations$CharGer_Classification),]
+truncations=truncations[!is.na(truncations$Overall_Classification),]
 p = ggplot(truncations,aes(x=transcript_quantile, y =expressionQuantile))#, color=Variant_Classification))
-p = p + facet_grid(.~CharGer_Classification,drop=T)
+p = p + facet_grid(.~Overall_Classification,drop=T)
 #p = p + geom_point(alpha=0.1) 
 p = p + theme_bw() + theme_nogrid() #+ guides(color=FALSE)
 #p = p + geom_abline(intercept = 0, slope=1, alpha=0.2) #+ geom_density2d(alpha=0.5)
@@ -66,7 +79,7 @@ p = p + theme_bw() + theme_nogrid() #+ guides(color=FALSE)
 #p = p + stat_density2d(aes(fill = ..level..),geom="tile")
 p = p + stat_density2d(aes(fill = ..level..),size = 0.01, bins = 32,  geom="polygon")
 p = p + scale_fill_gradientn(colours=getPalette(1000))
-#p = p + scale_fill_manual(values =  brewer.pal(length(unique(truncations$CharGer_Classification)),"Set1"))
+#p = p + scale_fill_manual(values =  brewer.pal(length(unique(truncations$Overall_Classification)),"Set1"))
 p = p + theme(axis.title = element_text(size=16), axis.text.x = element_text(colour="black", size=14, angle=90), axis.text.y = element_text(colour="black", size=14))#element_text(colour="black", size=14))
 p = p + coord_equal()
 p
@@ -74,21 +87,10 @@ fn = "out/pathVar_trunc_exp_vs_transcript_quantile.pdf"
 ggsave(file=fn, useDingbats=FALSE)
 
 # look into both the truncation location and the AI relationship to expression 
-truncations_exp = truncations[!is.na(truncations$expressionQuantile),]
-fn = paste(pd, 'pan8000_MAF0.05_trunc_exp_AI_vs_transcript_location.pdf',sep ="_")
-p = ggplot(truncations_exp,aes(x=transcript_quantile, y = tumor_by_normal_VAF, color=expressionQuantile))
-p = p + facet_grid(.~CharGer_Classification,drop=T)
-p = p + geom_point(alpha=0.3) + theme_bw() + theme_nogrid() 
-p = p + scale_color_gradientn(colours=getPalette(1000))
-p = p + theme(axis.title = element_text(size=16), axis.text.x = element_text(colour="black", size=14, angle=90), axis.text.y = element_text(colour="black", size=14))#element_text(colour="black", size=14))
-p = p + coord_equal()
-p
-ggsave(file=fn, width=16, useDingbats=FALSE)
-
 p = ggplot(truncations[truncations$HUGO_Symbol == "BRCA2",],aes(x=bp_to_end, y =expressionQuantile, color=Variant_Classification))
-p = p + facet_grid(.~CharGer_Classification,drop=T)
+p = p + facet_grid(.~Overall_Classification,drop=T)
 p = p + geom_point(alpha=0.8) + theme_bw() + theme_nogrid() #+ guides(color=FALSE)
-p = p + scale_fill_manual(values =  brewer.pal(length(unique(truncations$CharGer_Classification)),"Set1"))
+p = p + scale_fill_manual(values =  brewer.pal(length(unique(truncations$Overall_Classification)),"Set1"))
 p = p + theme(axis.title = element_text(size=16), axis.text.x = element_text(colour="black", size=14, angle=90), axis.text.y = element_text(colour="black", size=14))#element_text(colour="black", size=14))
 p = p + geom_vline(xintercept=50)
 p = p + scale_x_log10()
@@ -96,11 +98,10 @@ p
 fn = 'out/trunc_exp_vs_transcript_location_BRCA2.pdf'
 ggsave(file=fn, width=8, useDingbats=FALSE)
 
-fn = paste(pd, 'pan8000_MAF0.05_trunc_exp_vs_transcript_location_BRCA1.pdf',sep ="_")
 p = ggplot(truncations[truncations$HUGO_Symbol == "BRCA1",],aes(x=bp_to_end, y =expressionQuantile, color=Variant_Classification))
-p = p + facet_grid(.~CharGer_Classification,drop=T)
+p = p + facet_grid(.~Overall_Classification,drop=T)
 p = p + geom_point(alpha=0.8) + theme_bw() + theme_nogrid() #+ guides(color=FALSE)
-p = p + scale_fill_manual(values =  brewer.pal(length(unique(truncations$CharGer_Classification)),"Set1"))
+p = p + scale_fill_manual(values =  brewer.pal(length(unique(truncations$Overall_Classification)),"Set1"))
 p = p + theme(axis.title = element_text(size=16), axis.text.x = element_text(colour="black", size=14, angle=90), axis.text.y = element_text(colour="black", size=14))#element_text(colour="black", size=14))
 p = p + geom_vline(xintercept=50)
 p = p + scale_x_log10()
@@ -110,21 +111,21 @@ ggsave(file=fn, width=8, useDingbats=FALSE)
 
 # fit a linear model
 # how does the association changes with different type of truncations
-fit = lm(expressionQuantile ~ CharGer_Classification + transcript_quantile + Variant_Classification + LOH_Sig, data=truncations) 
+fit = lm(expressionQuantile ~ Overall_Classification + transcript_quantile + Variant_Classification + LOH_Sig, data=truncations) 
 summary(fit)
 fit = lm(expressionQuantile ~ transcript_quantile, data=truncations) 
 summary(fit)
 
-fit = lm(expressionQuantile ~ CharGer_Classification + bp_to_end + Variant_Classification, data=truncations) 
+fit = lm(expressionQuantile ~ Overall_Classification + bp_to_end + Variant_Classification, data=truncations) 
 summary(fit)
 
-fit = lm(expressionQuantile ~ CharGer_Classification + bp_boolean + Variant_Classification, data=truncations) 
+fit = lm(expressionQuantile ~ Overall_Classification + bp_boolean + Variant_Classification, data=truncations) 
 summary(fit)
 for (type in unique(truncations$Variant_Classification)){
   truncations_type = truncations[truncations$Variant_Classification == type, ]
-  fit = lm(expressionQuantile ~ CharGer_Classification + bp_boolean, data=truncations_type) 
+  fit = lm(expressionQuantile ~ Overall_Classification + bp_boolean, data=truncations_type) 
   summary(fit)
 }
 
-fit = lm(expressionQuantile ~ CharGer_Classification + transcript_quantile + bp_boolean + Variant_Classification, data=truncations) 
+fit = lm(expressionQuantile ~ Overall_Classification + transcript_quantile + bp_boolean + Variant_Classification, data=truncations) 
 summary(fit)
