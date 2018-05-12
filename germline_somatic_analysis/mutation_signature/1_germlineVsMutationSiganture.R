e93f74bba6f299bcf0c5b141a785831faa3941cb
Kuan Huang <huangkuanlin@gmail.com>
2018-05-11 19:29:55 -0500

    added mutation signature to the germline_somatic folder

---
+++ germline_somatic_analysis/mutation_signature/1_germlineVsMutationSiganture.R
@@ -0,0 +1,142 @@
+##### germlineVsMutationSignature.R #####
+# Kuan-lin Huang 2018
+
+source("../global_aes_out.R")
+source("../dependency_files.R")
+
+### data
+
+
+#FUNCTION myglm#
+myglm=function(z,trait,variant,covar=NA,ytype) {
+  if (is.na(covar) | is.null(covar) | nchar(covar)==0  ) {
+    model=formula(paste(trait,"~",variant))
+  } else {
+    model=formula(paste(trait,"~",variant,"+",covar))
+  }
+  if (ytype=="B") fit=glm(formula=model,data=z,family=binomial(link = "logit"))
+  if (ytype=="Q") fit=glm(formula=model,data=z,family=gaussian(link = "identity"))
+  fit
+}
+#END myglm
+
+#FUNCTION plot_violin#
+### plot quantitative y in relationship to x
+### faceted by covariate covi
+plot_violin = function(y,yi,xi,covi){
+  # change height based on the number of markers
+  dat = y[,c(yi, xi, covi)]
+  dat[,1] = as.numeric(dat[,1])
+  #dat.m = melt(mut_exp, id.var="Status")
+  # plot violin plots faceted by marker genes
+  n_facet = length(unique(dat[,covi]))
+  p = ggplot(data=dat)
+  p = p + facet_grid(as.formula(paste(". ~", covi)))
+  p = p + geom_violin(aes_string(x=xi, y=yi, fill=xi),alpha=0.5) + guides(fill=FALSE, color =FALSE)
+  p = p + geom_jitter(aes_string(x=xi, y=yi, color=xi), alpha = 0.2) #+ geom_point(aes(x=Status, y=value))
+  p = p + guides(fill=FALSE, color =FALSE)
+  p = p + labs(x = paste(xi,"pathVarPline Variant"), y = yi) + theme_bw()
+  p = p + theme(text = element_text(colour="black", size=16), axis.text.x = element_text(colour="black", size=14),
+                axis.text.y = element_text(colour="black", size=14), strip.text = element_text(size = 8, angle = 90))
+  p
+  fn = paste(pd, yi, "by", xi, "cross", covi, "violin.pdf", sep="_")
+  ggsave(file=fn, width = n_facet, useDingbats=FALSE)
+}
+
+run_glm = function(data=NULL, covi="") {
+
+  ### data input #####
+  data_g = data#[, c("age_at_initial_pathologic_diagnosis","type",gene)]
+
+  ######### analysis ##########
+  row_stat=NULL
+
+  # analysis_type   clinical_data_trait_name    variant/gene_name   covariates  memo
+  ytype="Q";yi="Contribution";xi="Freq";covi=covi;#covi=md[i,4];memo=md[i,5]
+  # DEBUG
+  #cat(paste("    Processing: yi =", yi, " xi =", xi, " covi =", covi, "\n") )
+
+  if (ytype=="Q") {
+    test = "F"
+  } else if (ytype=="B") {
+    test = "Chisq"
+  } else {
+    stop("Unknown model ytype ", ytype)
+  }
+
+  glm = try(myglm(data_g,yi,xi,covi,ytype))  # MAW new
+  if(class(glm)[1] == "try-error") {
+    cat(paste("    Error caught, continuing.  yi =", yi, " xi =", xi, " covi =", covi, "\n") )
+    next
+  }
+  try(anova(glm,test=test))->fit
+
+#   if (xi %in% names(coefficients(glm))){
+#     coeff = coefficients(glm)[[xi]]
+  if (length(names(coefficients(glm)))>1){
+    coeff = coefficients(glm)[[2]]
+  } else {coeff = NA}
+
+  if (class(fit)[1]!="try-error")
+  {
+    fit=as.matrix(fit)
+    if (xi %in% rownames(fit)) (row_stat = cbind(yi,ytype,xi,as.data.frame(t(fit[xi,])),coeff,covi))
+  }
+  return(row_stat)
+
+}
+
+### MAIN ###
+
+# read input files
+gene_sample = data.frame(table(pathVarP$HUGO_Symbol,pathVarP$bcr_patient_barcode))
+colnames(gene_sample) = c("Gene","bcr_patient_barcode","Freq")
+
+mut_signature_f = "/Users/khuang/Box\ Sync/PhD/germline/PanCanAtlasGermline/TCGA_data/signature_profile_sample.txt.gz"
+mut_signature = read.table(sep="\t",header=T,file=gzfile(mut_signature_f), stringsAsFactors=FALSE)
+mut_signature_tcga = mut_signature[mut_signature$Country=="United States",]
+mut_signature_tcga$cancer = gsub("-US","",mut_signature_tcga$project_code)
+mut_signature_tcga$Signature = gsub("\\.","-",mut_signature_tcga$Signature)
+mut_signature_tcga$bcr_patient_barcode = gsub("\\.","-",mut_signature_tcga$Tumor_Sample_Barcode)
+
+mut_signature_tcga_brief = mut_signature_tcga[,c("cancer" , "Signature", "Contribution", "bcr_patient_barcode")]
+
+##### individual cancer type analysis #####
+cancers = unique(pathVarP$cancer)
+genes = unique(pathVarP$HUGO_Symbol)
+# limit runs to cancers with at least 5 likely patho/pathogenic variants
+tt = NULL
+for (gene in genes){
+  gene_sample_g = gene_sample[gene_sample$Gene==gene,]
+  var_exp_g = merge(mut_signature_tcga_brief,gene_sample_g,by="bcr_patient_barcode",all.x=T)
+  var_exp_g$Freq[is.na(var_exp_g$Freq)] = 0
+  var_exp_g$Freq[var_exp_g$Freq != 0 ] = 1
+  for (cancer in cancers){
+    var_exp_g_c = var_exp_g[var_exp_g$cancer %in% cancer,]
+    gene_path_count = sum(var_exp_g_c$Freq[var_exp_g_c$Signature=="Signature-1"])
+    if (gene_path_count > 2){
+      for (signature in unique(var_exp_g_c$Signature)){
+        var_exp_g_c_s = var_exp_g_c[var_exp_g_c$Signature == signature,]
+        # run GLM
+        w = wilcox.test(var_exp_g_c_s$Contribution[var_exp_g_c_s$Freq==0],var_exp_g_c_s$Contribution[var_exp_g_c_s$Freq!=0])
+        wP = w$p.value
+        wWstat = w$statistic
+        cancer_gene_stat = run_glm(var_exp_g_c_s)
+        # compile results
+        full_cancer_gene_stat = cbind(cancer,gene,signature,gene_path_count,wP,wWstat,cancer_gene_stat)
+        tt = rbind(tt, full_cancer_gene_stat)
+      }
+    }
+  }
+}
+
+#"yi","ytype","xi","Df","Deviance","Resid. Df","Resid. Dev","F","Pr(>F)","covi","memo"
+colnames(tt) = c("cancer","gene","signature","gene_path_count","wilcoxP","W_stat","y","y_type","Gene","degrees_freedom","deviance","residual_degrees_freedom","residual_deviance",
+                 "F_statistic","p-value","coefficient","covariants");
+tt$FDR = p.adjust(tt[,"p-value"], method="fdr") # MAW new, calculates FDR based on the method from,
+# Benjamini, Y., and Hochberg, Y. (1995). Controlling the false discovery rate: a practical and powerful approach to multiple testing. Journal of the Royal Statistical Society Series B 57, 289–300.
+tt$wilcoxFDR = p.adjust(tt[,"wilcoxP"], method="fdr")
+
+tt=tt[order(tt$FDR, decreasing=FALSE),]
+tn = "out/pathVarMutsigAssoc.txt"
+write.table(tt, quote=F, sep="\t", file = tn, row.names = F)
---
+++ germline_somatic_analysis/mutation_signature/2_plotPathVarMutsigAssoc.R
@@ -0,0 +1,86 @@
+##### plotPathVarMutsigAssoc.R #####
+# Kuan-lin Huang 2018
+
+source("../global_aes_out.R")
+source("../dependency_files.R")
+
+tn = "out/pathVarMutsigAssoc.txt"
+tt = read.table(sep="\t",header=T,file=tn, stringsAsFactors=FALSE)
+
+### plotting ###
+tt$signature = factor(tt$signature)
+tt$signature = factor(tt$signature,levels = c("Signature-1","Signature-2","Signature-3","Signature-4","Signature-5","Signature-6"
+                                              ,"Signature-7","Signature-8","Signature-9","Signature-10","Signature-11","Signature-12"
+                                              ,"Signature-13","Signature-14","Signature-15","Signature-16","Signature-17","Signature-18"
+                                              ,"Signature-19","Signature-20","Signature-21","Signature-22","Signature-23","Signature-24"
+                                              ,"Signature-25","Signature-26","Signature-27","Signature-28","Signature-29","Signature-30"))
+tt$association = "None"
+tt$association[tt$FDR<0.15] = "Suggestive"
+tt$association[tt$FDR<0.05] = "Significant"
+tt$gene = as.character(tt$gene)
+tt$FDR_plot = -log(tt$FDR)
+tt$FDR_plot[tt$FDR_plot > 5 ] = 5
+uniqG = unique(tt$gene[tt$FDR<0.05])
+ttG = tt[tt$gene %in% uniqG,]
+
+getPalette = colorRampPalette(c("#FFFFFF","#fed976","#e31a1c"))
+p = ggplot(data=ttG)
+p = p + facet_grid(gene~.,space="free",scale="free")
+p = p + geom_tile(data=ttG,aes(y=cancer, x=signature, fill= coefficient), linetype="blank") + scale_fill_gradientn(name= "Coefficient", colours=getPalette(100), na.value=NA, limit=c(0,NA))
+#p = p + geom_text(data=ttG,aes(x=cancer, y=signature, label = coefficient), color="black", size=3)
+p = p + geom_tile(data=ttG[ttG$FDR < 0.15,],aes(y=cancer, x=signature), color="grey",fill=NA, size=1.5) #+ scale_color_gradientn(name= "Sig", colours=sigColors)
+p = p + geom_tile(data=ttG[ttG$FDR < 0.05,],aes(y=cancer, x=signature), color="black",fill=NA, size=1.5) #+ scale_color_gradientn(name= "Sig", colours=sigColors)
+p = p  + theme_bw() + theme_nogrid() +
+  theme(axis.title = element_blank(), axis.text.x = element_text(colour="black", size=12, angle=90, vjust = 0.5), axis.text.y = element_text(colour="black", size=12),axis.ticks = element_blank())#element_text(colour="black", size=14))
+p + labs(x="Signature",y = "Cancer")
+
+fn = 'out/pan10389_germlineAssocWithmutSignatureHeatmap.pdf'
+ggsave(fn,h=8,useDingbat=F)
+
+# # using GLM test result
+# tt$FDR_plot[tt$FDR_plot<10^(-6)]= 0.95*10^(-6)
+# p = ggplot(data=tt)
+# p = p + geom_point(aes(y=-log10(FDR_plot),x= coefficient,color = cancer),alpha=0.5)
+# #p = p + geom_text_repel(aes(y=-log10(FDR),x= cohort_AF,label=ifelse(FDR<0.05, Gene,NA),color = Cancer))#,alpha=1.3)
+# #p = p + geom_point(aes(y=cohort_AF,x=Cancer,size=-log10(FDR),color = Cancer))
+# p = p + geom_text_repel(aes(y=-log10(FDR_plot),x= coefficient,color = cancer,label=ifelse(FDR<0.05, gene,NA)))
+# p = p + getPCACancerColor()
+# p = p + labs(x="Coefficient",y= "-log10(FDR)")
+# p = p + geom_vline(xintercept = 0, alpha=0.5)
+# p = p  + theme_bw() +
+#   theme(axis.text.x = element_text(colour="black", size=12), axis.text.y = element_text(colour="black", size=12),axis.ticks = element_blank())#element_text(colour="black", size=14))
+# p
+# fn = 'out/geneExpressAssocVolcanoGLM.pdf'
+# ggsave(fn,w = 5, h = 5, useDingbat=F)
+
+# # using the Wilcox test result
+# p = ggplot(data=tt)
+# p = p + geom_point(aes(y=-log10(wilcoxFDR),x= coefficient,color = cancer),alpha=0.5)
+# #p = p + geom_text_repel(aes(y=-log10(FDR),x= cohort_AF,label=ifelse(FDR<0.05, Gene,NA),color = Cancer))#,alpha=1.3)
+# #p = p + geom_point(aes(y=cohort_AF,x=Cancer,size=-log10(FDR),color = Cancer))
+# p = p + geom_text_repel(aes(y=-log10(wilcoxFDR),x= coefficient,color = cancer,label=ifelse(FDR<0.05, gene,NA)))
+# p = p + getPCACancerColor()
+# p = p + labs(x="Coefficient",y= "-log10(FDR)")
+# p = p + geom_vline(xintercept = 0, alpha=0.5)
+# p = p  + theme_bw() +
+#   theme(axis.text.x = element_text(colour="black", size=12), axis.text.y = element_text(colour="black", size=12),axis.ticks = element_blank())#element_text(colour="black", size=14))
+# p
+# fn = 'out/geneExpressAssocVolcanoWCOX.pdf'
+# ggsave(fn,w = 5, h = 5, useDingbat=F)
+
+#tt$association = factor(tt$association,level=c("None","Suggestive","Significant"))
+
+# plot by gene
+p = ggplot(data=ttG,aes(x=coefficient,y=cancer,color = cancer))
+p = p + facet_grid(gene~.,space="free",scale="free")
+# p = p + geom_point(aes(y=-log10(wilcoxFDR),x= coefficient,color = cancer),alpha=0.5)
+p = p + geom_point(aes(shape=association),alpha=0.3,size=2)
+p = p + geom_text_repel(aes(label=ifelse(FDR<0.05,as.character(signature),NA)))
+p = p + getPCACancerColor()
+p = p + labs(x="Cancer",y= "-log10(FDR)")
+p = p + geom_vline(xintercept = 0, alpha=0.5) #+ xlim(-3.1,3.1)
+p = p  + theme_bw() +
+  theme(axis.text.x = element_text(colour="black", size=12), axis.text.y = element_text(colour="black", size=12),axis.ticks = element_blank())#element_text(colour="black", size=14))
+p + labs(x = "coefficient",y="cancer")
+fn = 'out/pan10389_germlineAssocWithmutSignatureByGene.pdf'
+ggsave(fn,h=8,useDingbat=F)
